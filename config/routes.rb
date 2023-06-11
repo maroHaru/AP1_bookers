51b13f67a45fc17144c89a2630da31b10def158a
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-06-11 13:42:23 +0000

    [Add] 3-15

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'books' => 'books#index'
-  get 'books/show'
-  get 'books/edit'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   post 'books' => 'books#create'
   get 'books/new'
   get '/' => 'homes#top'
